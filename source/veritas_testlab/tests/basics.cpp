156bc315cc075216ee1659a8fdaf9db12b047ae7
dgregorius <dgregorius@a963baa4-12c8-497c-b908-45a22ef3257c>
2026-01-11 22:37:37 +0000

    git-svn-id: https://dgregorius.svn.beanstalkapp.com/veritas/trunk@64 a963baa4-12c8-497c-b908-45a22ef3257c

--- source/veritas_testlab/tests/basics.cpp
+++ source/veritas_testlab/tests/basics.cpp
@@ -18,25 +18,80 @@ class VsBasicScene1 : public VsTest
 			{
 			IVsHull* Ground = mPlugin->CreateBox( VsVector3( 15.0f, 1.0f, 15.0f ) );
 			IVsBody* GroundBody = mWorld->CreateBody( VS_STATIC_BODY );
-			if ( GroundBody )
-				{
-				GroundBody->SetPosition( VsVector3( 0.0f, -1.0f, 0.0f ) );
-				if ( IVsShape* GroundShape = GroundBody->CreateHull( Ground ) )
-					{
-					GroundShape->SetColor( { 0.3f, 0.3f, 0.3f, 1.0f } );
-					}
-				
-				}
+			GroundBody->SetPosition( VsVector3( 0.0f, -1.0f, 0.0f ) );
+			IVsShape* GroundShape = GroundBody->CreateHull( Ground );
+			GroundShape->SetColor( { 0.3f, 0.3f, 0.3f, 1.0f } );
+
 
 			IVsHull* Box = mPlugin->CreateBox( VsVector3( 1.0f, 1.0f, 1.0f ) );
 			IVsBody* BoxBody = mWorld->CreateBody( VS_DYNAMIC_BODY );
-			if ( BoxBody )
+			BoxBody->SetPosition( VsVector3( 0.0f, 15.0f, 0.0f ) );
+			BoxBody->CreateHull( Box );
+			}
+	};
+
+// Registry
+VS_DEFINE_TEST( "Basics", "Scene1 - Falling Box", VsBasicScene1 );
+
+
+//--------------------------------------------------------------------------------------------------
+// VsBasicScene2 - Small stack
+//--------------------------------------------------------------------------------------------------
+class VsBasicScene2 : public VsTest
+	{
+	using VsTest::VsTest;
+
+	public:
+		virtual void Create( VsCamera* Camera ) override
+			{
+			IVsHull* Ground = mPlugin->CreateBox( VsVector3( 15.0f, 1.0f, 15.0f ) );
+			IVsBody* GroundBody = mWorld->CreateBody( VS_STATIC_BODY );
+			GroundBody->SetPosition( VsVector3( 0.0f, -1.0f, 0.0f ) );
+			IVsShape* GroundShape = GroundBody->CreateHull( Ground );
+			GroundShape->SetColor( { 0.3f, 0.3f, 0.3f, 1.0f } );
+
+			IVsHull* Box = mPlugin->CreateBox( VsVector3( 1.0f, 1.0f, 1.0f ) );
+			for ( int Index = 0; Index < 5; ++Index )
 				{
-				BoxBody->SetPosition( VsVector3( 0.0f, 15.0f, 0.0f ) );
+				IVsBody* BoxBody = mWorld->CreateBody( VS_DYNAMIC_BODY );
+				BoxBody->SetPosition( VsVector3( 0.0f, 2.5f * Index + 1.5f, 0.0f ) );
 				BoxBody->CreateHull( Box );
 				}
 			}
 	};
 
 // Registry
-VS_DEFINE_TEST( "Basics", "Scene1 - Falling Box", VsBasicScene1 );
+VS_DEFINE_TEST( "Basics", "Scene2 - Small stack", VsBasicScene2 );
+
+
+//--------------------------------------------------------------------------------------------------
+// VsBasicScene3 - Small pyramid
+//--------------------------------------------------------------------------------------------------
+class VsBasicScene3 : public VsTest
+	{
+	using VsTest::VsTest;
+
+	public:
+		virtual void Create( VsCamera* Camera ) override
+			{
+			IVsHull* Ground = mPlugin->CreateBox( VsVector3( 25.0f, 1.0f, 25.0f ) );
+			IVsBody* GroundBody = mWorld->CreateBody( VS_STATIC_BODY );
+			GroundBody->SetPosition( VsVector3( 0.0f, -1.0f, 0.0f ) );
+			IVsShape* GroundShape = GroundBody->CreateHull( Ground );
+			GroundShape->SetColor( { 0.3f, 0.3f, 0.3f, 1.0f } );
+
+			IVsHull* Box = mPlugin->CreateBox( VsVector3( 1.0f, 1.0f, 1.0f ) );
+			for ( int Row = 0; Row < 12; ++Row )
+				{
+				for ( int Column = 0; Column < 12 - Row; ++Column )
+					{
+					IVsBody* BoxBody = mWorld->CreateBody( VS_DYNAMIC_BODY );
+					BoxBody->SetPosition( VsVector3( -12.0f + 2.0f * Column + Row, 1.5f + 2.5f * Row, 0.0f ) );
+					BoxBody->AddHull( mBox );
+					}
+				}
+			}
+	};
+
+// Registry
+VS_DEFINE_TEST( "Basics", "Scene2 - Small stack", VsBasicScene2 );
